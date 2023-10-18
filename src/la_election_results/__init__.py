efdb0a72b10ebe14b01e446f55d2dfdf5808d19a
Charlie Bini <5003326+cbini@users.noreply.github.com>
2023-10-18 03:31:00 +0000

    rm data

--- src/la_election_results/__init__.py
+++ src/la_election_results/__init__.py
@@ -4,35 +4,192 @@ import pathlib
 import pendulum
 import requests
 
-BASE_URL = "https://voterportal.sos.la.gov/ElectionResults/ElectionResults/Data"
+
+class ElectionResultsClient(requests.Session):
+    def __init__(self) -> None:
+        super().__init__()
+
+        self.base_url = (
+            "https://voterportal.sos.la.gov/ElectionResults/ElectionResults/Data"
+        )
+
+    def get_response_json(self, **kwargs):
+        response = self.get(url=self.base_url, params=kwargs)
+
+        response.raise_for_status()
+
+        return response.json()
+
+    def election_dates(self):
+        return self.get_response_json(blob="ElectionDates.htm")
+
+    def parishes_in_election(self, election_date):
+        return self.get_response_json(blob=f"{election_date}/ParishesInElection.htm")
+
+    def races_candidates_multiparish(self, election_date):
+        return self.get_response_json(
+            blob=f"{election_date}/RacesCandidates_Multiparish.htm"
+        )
+
+    def votes_multiparish(self, election_date):
+        return self.get_response_json(blob=f"{election_date}/Votes_Multiparish.htm")
+
+    def votes_race_by_parish(self, election_date, race_id):
+        return self.get_response_json(
+            blob=f"{election_date}/VotesRaceByParish/Votes_{race_id}.htm"
+        )
+
+    def races_candidates_by_parish(self, election_date, parish_value):
+        return self.get_response_json(
+            blob=f"{election_date}/RacesCandidates/ByParish_{parish_value}.htm"
+        )
+
+    def votes_parish(self, election_date, parish_value):
+        return self.get_response_json(
+            blob=f"{election_date}/VotesParish/Votes_{parish_value}.htm"
+        )
+
+    def votes_race_by_precinct(self, election_date, race_id, parish_value):
+        return self.get_response_json(
+            blob=(
+                f"{election_date}/VotesRaceByPrecinct/"
+                f"Votes_{race_id}_{parish_value}.htm"
+            )
+        )
 
 
-def get_response_json(url, **kwargs):
-    response = requests.get(url=url, params=kwargs)
-
-    response.raise_for_status()
-
-    return response.json()
-
-
-def download_blob_data(filepath, blob):
+def save(filepath, data):
+    print(f"\tSaving to {filepath}...")
     filepath.parent.mkdir(parents=True, exist_ok=True)
-    print(f"\tDownloading to {filepath}...")
-
-    data = get_response_json(url=BASE_URL, blob=blob)
-
     json.dump(obj=data, fp=filepath.open(mode="w"))
 
-    return data
+
+def foo(client: ElectionResultsClient, data_dir: pathlib.Path, election_date: str):
+    # ParishesInElection
+    save(
+        filepath=(
+            data_dir
+            / "parishes_in_election"
+            / f"_election_date={election_date}"
+            / "data.json"
+        ),
+        data=client.parishes_in_election(),
+    )
+
+    # RacesCandidates_Multiparish
+    save(
+        filepath=(
+            data_dir
+            / "races_candidates"
+            / f"_election_date={election_date}"
+            / "_level=multiparish"
+            / "_parish=all"
+            / "data.json"
+        ),
+        data=client.races_candidates_multiparish(),
+    )
+
+    # Votes_Multiparish
+    votes_multiparish_data = download_blob_data(
+        filepath=(
+            data_dir
+            / "votes"
+            / f"_election_date={election_date}"
+            / "_level=multiparish"
+            / "_race=all"
+            / "_parish=all"
+            / "data.json"
+        ),
+        blob=f"{election_date}/Votes_Multiparish.htm",
+    )
+
+    # instantiate parish_values for later
+    parish_values = set()
+
+    # ensure races is list
+    races = votes_multiparish_data["Races"].get("Race", [])
+    if not isinstance(races, list):
+        races = [races]
+
+    for race in races:
+        race_id = race["ID"]
+
+        # VotesRaceByParish/Votes_{RaceID}
+        votes_race_by_parish_data = download_blob_data(
+            filepath=(
+                data_dir
+                / "votes"
+                / f"_election_date={election_date}"
+                / "_level=parish"
+                / f"_race={race_id}"
+                / "_parish=all"
+                / "data.json"
+            ),
+            blob=f"{election_date}/VotesRaceByParish/Votes_{race_id}.htm",
+        )
+
+        # ensure parishes is list
+        parishes = votes_race_by_parish_data["Parishes"].get("Parish", [])
+        if not isinstance(parishes, list):
+            parishes = [parishes]
+
+        for parish in parishes:
+            parish_value = parish["ParishValue"]
+            parish_values.add(parish_value)
+
+            # {ElectionDate}/VotesRaceByPrecinct/Votes_{RaceID}_{ParishValue}
+            download_blob_data(
+                filepath=(
+                    data_dir
+                    / "votes"
+                    / f"_election_date={election_date}"
+                    / "_level=precinct"
+                    / f"_race={race_id}"
+                    / f"_parish={parish_value}"
+                    / "data.json"
+                ),
+                blob=(
+                    f"{election_date}/VotesRaceByPrecinct/"
+                    f"Votes_{race_id}_{parish_value}.htm"
+                ),
+            )
+
+    for parish_value in parish_values:
+        # RacesCandidates/ByParish_{ParishValue}
+        download_blob_data(
+            filepath=(
+                data_dir
+                / "races_candidates"
+                / f"_election_date={election_date}"
+                / "_level=parish"
+                / f"_parish={parish_value}"
+                / "data.json"
+            ),
+            blob=f"{election_date}/RacesCandidates/ByParish_{parish_value}.htm",
+        )
+
+        # VotesParish/Votes_{ParishValue}
+        download_blob_data(
+            filepath=(
+                data_dir
+                / "votes"
+                / f"_election_date={election_date}"
+                / "_level=parish"
+                / "_race=all"
+                / f"_parish={parish_value}"
+                / "data.json"
+            ),
+            blob=f"{election_date}/VotesParish/Votes_{parish_value}.htm",
+        )
 
 
 def main():
+    client = ElectionResultsClient()
     data_dir = pathlib.Path("./data")
 
     # ElectionDates
-    election_dates_data = download_blob_data(
-        filepath=data_dir / "election_dates" / "data.json", blob="ElectionDates.htm"
-    )
+    election_dates_data = client.election_dates()
+    save(filepath=data_dir / "election_dates" / "data.json", blob=election_dates_data)
 
     for date in election_dates_data["Dates"]["Date"]:
         election_date = pendulum.from_format(
@@ -124,7 +281,10 @@ def main():
                         / f"_parish={parish_value}"
                         / "data.json"
                     ),
-                    blob=f"{election_date_fmt}/VotesRaceByParish/Votes_{race_id}.htm",
+                    blob=(
+                        f"{election_date_fmt}/VotesRaceByPrecinct/"
+                        f"Votes_{race_id}_{parish_value}.htm"
+                    ),
                 )
 
         for parish_value in parish_values:
