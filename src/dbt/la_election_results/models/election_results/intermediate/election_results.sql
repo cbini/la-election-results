7a366857c6d3b0d3744fd579b205eb24ca647d91
Charlie Bini <5003326+cbini@users.noreply.github.com>
2023-10-22 22:24:54 +0000

    add

---
+++ src/dbt/la_election_results/models/election_results/intermediate/election_results.sql
@@ -0,0 +1,63 @@
+select
+    ed.pk_election_id,
+    ed.election_date,
+    ed.rep_map_name,
+    ed.cong_map_name,
+    ed.sen_map_name,
+    ed.turnout_message,
+    ed.order,
+    ed.results_official,
+    ed.pwp_stats_exist,
+    ed.race_stats_exist,
+    ed.post_election_stats_available,
+    ed.display_turnout_stats,
+
+    vp.race_id,
+    vp.parish_value,
+    vp.precinct,
+    vp.has_reported,
+    vp.choice_id,
+    vp.voter_count_qualified,
+    vp.voter_count_voted,
+    vp.choice_vote_total,
+
+    pie.parish_name
+    pie.precincts,
+    pie.precincts_reported,
+    pie.complete_with_absentee,
+
+    coalesce(rcp.office_level, rcm.office_level) as office_level,
+    coalesce(rcp.general_title, rcm.general_title) as general_title,
+    coalesce(rcp.specific_title, rcm.specific_title) as specific_title,
+    coalesce(rcp.summary_text, rcm.summary_text) as summary_text,
+    coalesce(rcp.full_text, rcm.full_text) as full_text,
+    coalesce(rcp.choice_description, rcm.choice_description) as choice_description,
+    coalesce(rcp.choice_party, rcm.choice_party) as choice_party,
+    coalesce(rcp.choice_color, rcm.choice_color) as choice_color,
+    coalesce(rcp.is_closed_party, rcm.is_closed_party) as is_closed_party,
+    coalesce(rcp.is_multi_parish, rcm.is_multi_parish) as is_multi_parish,
+    coalesce(
+        rcp.is_presidential_nominee, rcm.is_presidential_nominee
+    ) as is_presidential_nominee,
+    coalesce(
+        rcp.number_to_be_elected, rcm.number_to_be_elected
+    ) as number_to_be_elected,
+from {{ ref("election_dates") }} as ed
+inner join {{ ref("votes_precinct") }} as vp on ed.election_date = vp.election_date
+inner join
+    {{ ref("parishes_in_election") }} as pie
+    on vp.parish_value = pie.parish_value
+    and vp.election_date = pie.election_date
+left join
+    {{ ref("races_candidates") }} as rcp
+    on vp.election_date = rcp.election_date
+    and vp.race_id = rcp.race_id
+    and vp.parish_value = rcp.parish_value
+    and vp.choice_id = rcp.choice_id
+    and rcp.level = 'parish'
+left join
+    {{ ref("races_candidates") }} as rcm
+    on vp.election_date = rcm.election_date
+    and vp.race_id = rcm.race_id
+    and vp.choice_id = rcm.choice_id
+    and rcm.level = 'multiparish'
--- src/dbt/la_election_results/models/election_results/staging/parishes_in_election.sql
+++ src/dbt/la_election_results/models/election_results/staging/parishes_in_election.sql
@@ -1,10 +1,19 @@
-select
-    pie.parishesinelection.versiondatetime as version_date_time,
-    parse_date('%Y%m%d', safe_cast(pie._election_date as string)) as election_date,
+with
+    parishes_in_election as (
+        select
+            pie.parishesinelection.versiondatetime as version_date_time,
+            parse_date(
+                '%Y%m%d', safe_cast(pie._election_date as string)
+            ) as election_date,
 
-    p.precincts as precincts,
-    p.precinctsreported as precincts_reported,
-    right(concat('0', p.parishvalue), 2) as parish_value,
-    if(p.completewithabsentee = 1, true, false) as complete_with_absentee,
-from {{ source("election_results", "parishes_in_election") }} as pie
-cross join unnest(pie.parishesinelection.parish) as p
+            p.precincts as precincts,
+            p.precinctsreported as precincts_reported,
+            right(concat('0', p.parishvalue), 2) as parish_value,
+            if(p.completewithabsentee = 1, true, false) as complete_with_absentee,
+        from {{ source("election_results", "parishes_in_election") }} as pie
+        cross join unnest(pie.parishesinelection.parish) as p
+    )
+
+select pie.*, pm.name as parish_name,
+from parishes_in_election as pie
+inner join {{ ref("parish_metadata") }} as pm on pie.parish_value = pm.id
